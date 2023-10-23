0b2d36960cb26a172099bf46a9347a33503085d3
Paul <paullush@hotmail.com>
2023-10-24 12:34:50 +1300

    delete and redownload local files that dont match remote size

--- clone.php
+++ clone.php
@@ -1,5 +1,6 @@
 <?php
-function getLinks($url, $dir) {
+function getLinks($url) {
+
     $html = file_get_contents($url);
     $dom = new DOMDocument;
     @$dom->loadHTML($html);
@@ -11,10 +12,34 @@ function getLinks($url, $dir) {
             $remote = parse_url($node->getAttribute("href"));
             $fullpath = explode("/", $remote["path"]);
             $filename = urldecode(array_pop($fullpath));
-            $path = "download/Os/DSM/$dir/";
+            $path = "download/Os/DSM/";
 
             @mkdir($path, 0777, true);
-            $dest = "$path$filename";            
+            $dest = "$path$filename";
+
+	        // getting remote file size
+            $ch = curl_init($node->getAttribute("href"));
+            curl_setopt($ch, CURLOPT_NOBODY, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, true);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+            $contentLength = 'unknown';
+            $status = 'unknown';
+            if (preg_match('/^HTTP\/(1\.[01]|2) (\d\d\d)/', $data, $matches)) {
+                 $status = (int)$matches[2];
+            }
+            if (preg_match('/Content-Length: (\d+)/i', $data, $matches)) {
+                 $contentLength = (int)$matches[1];
+            }
+
+            if ($status == 200 && $contentLength <> "unknown" && file_exists($dest) && (filesize($dest) != $contentLength)) {
+                echo "remote file is $contentLength however local file is " . filesize($dest) . ", redownloading\n";
+                @unlink($dest);
+            }
+
             if (!file_exists($dest)) {
                 echo "downloading " . $node->getAttribute("href") . " to $dest \n";
                 $fh = fopen($dest, "w");
